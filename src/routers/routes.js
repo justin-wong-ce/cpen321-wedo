3ea22832e5443a4b4d1e8dc073b936a2d50e5692
Ubuntu <cpen321@cpen321-wedo.walwv55x2eoe3p3q3yntzpzqtf.dx.internal.cloudapp.net>
2020-10-26 14:41:42 +0000

    fixed buge in routes.js

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -29,18 +29,11 @@ router.get('/routes/transit', (req, res) => {
 
 // Returns a driving route with locations converted to waypoints
 async function getDrivingRoute(locations) {
-
-    console.log("getting driving route");
-    console.log(locations);
-
     if (locations.length != 2) {
         var waypoints = ["optimize:true"];
         for (let iter = 1; iter < locations.length - 1; iter++) {
             waypoints.push(locations[iter]);
         }
-
-        console.log(waypoints);
-
         return client.directions({
             params: {
                 origin: locations[0],
@@ -85,17 +78,11 @@ async function getTransitRoute(locations, distanceThreshold) {
     var farApartCoor = [];
     var closeByGroups = [];
 
-    console.log("get groupByDistance");
     groupByDistance(farApartCoor, closeByGroups, response.data.routes[0], distanceThreshold);
-    console.log("done groupByDistance");
-    console.log("check if proper trasaction: ", farApartCoor, closeByGroups);
 
     // Get transit time needed for each far apart location
     // Return empty array on error (error logged in function already)
-    console.log("getBestTransitRoutes");
     var transitRoutes = await getBestTransitRoutes(farApartCoor);
-    console.log("got the best transit routes!");
-    console.log(transitRoutes);
 
     let transitRouteIter = 0;
 
@@ -103,15 +90,11 @@ async function getTransitRoute(locations, distanceThreshold) {
         res = null;
         return;
     }
-    console.log("transitRoutes:", transitRoutes);
 
     var results = [];
 
-    console.log("closeByGroups", closeByGroups);
-
     // Make array
     for (let closeGroup of closeByGroups) {
-        console.log("closeGroup", closeGroup);
         var pointGroup = [];
         if (closeGroup.length > 2) {
             for (let pointIter = 1; pointIter < closeGroup.length - 2; pointIter++) {
@@ -132,6 +115,7 @@ async function getTransitRoute(locations, distanceThreshold) {
             results.push(response.data.routes[0]);
         }
         if (transitRouteIter < transitRoutes.length) {
+
             let response = await client.directions({
                 params: {
                     origin: toUrlValue(transitRoutes[transitRouteIter].routes[0].legs[0].start_location),
@@ -145,8 +129,6 @@ async function getTransitRoute(locations, distanceThreshold) {
             transitRouteIter++;
         }
     }
-
-    console.log(results);
 
     return results;
 }
@@ -185,26 +167,12 @@ function groupByDistance(farApartCoor, closeByGroups, route, walkDistanceThresho
 async function getBestTransitRoutes(locations) {
     // Basically a travelling postman problem
     // Using naive approach for now
-
-    // Make array without starting location, with dummy location
-    let noStartingLocation = [];
-    let startingLoc = locations[0].toString();
-    let endingLoc = locations[locations.length - 1].toString();
-    for (let locationNumber = 1; locationNumber < locations.length; locationNumber++) {
-        noStartingLocation.push(locations[locationNumber].toString());
-    }
-    // Insert dummy address to achieve custom ending node, instead of returning to starting point
-    noStartingLocation.push("DUMMY");
-
-    // Get array of permuations of locations
-    let permutations = permutate(noStartingLocation);
-
     // get transit route for every possible combination
     const locationGraph = new Map();
     const responsesMap = new Map();
 
+    let noStartingLocation = [];
     for (let locIter0 = 0; locIter0 < locations.length - 1; locIter0++) {
-
         var neighbours = new Map();
 
         for (let locIter1 = 1; locIter1 < locations.length; locIter1++) {
@@ -218,12 +186,23 @@ async function getBestTransitRoutes(locations) {
                 },
                 timeout: 2000
             });
+
             // can make entire inner for loop asynchronously later on
-            neighbours.set(response.data.routes[0].legs[0].end_location.toString(), response.data.routes[0].legs[0].distance.value);
-            responsesMap.set(locations[locIter0].toString() + locations[locIter1].toString(), response.data);
+            neighbours.set(locIter1.toString(), response.data.routes[0].legs[0].distance.value);
+            responsesMap.set(locIter0.toString() + locIter1.toString(), response.data);
         }
-        locationGraph.set(locations[locIter0].toString(), neighbours);
+        locationGraph.set(locIter0.toString(), neighbours);
     }
+    for (let count = 0; count < locations.length; count++) {
+        noStartingLocation.push(count.toString());
+    }
+    let startingLoc = noStartingLocation[0];
+    let endingLoc = noStartingLocation[noStartingLocation.length - 1];
+    noStartingLocation.push("-1");
+    noStartingLocation.shift();
+
+    // Get array of permuations of locations
+    let permutations = permutate(noStartingLocation);
 
     // Get optimal path
     let minDistance = Number.MAX_VALUE;
@@ -233,7 +212,7 @@ async function getBestTransitRoutes(locations) {
     do {
         let currPerm = permutations[onPermutation];
         onPermutation++;
-        if (currPerm[currPerm.length - 1] !== "DUMMY" || currPerm[currPerm.length - 2] !== endingLoc) {
+        if (currPerm[currPerm.length - 1] !== "-1" || currPerm[currPerm.length - 2] !== endingLoc) {
 
             continue;
         }
@@ -243,19 +222,16 @@ async function getBestTransitRoutes(locations) {
         let currWeight = 0;
         let currLoc = startingLoc;
         for (let iter = 0; iter < currPerm.length; iter++) {
-            if (currPerm[iter] === "DUMMY" && currLoc != endingLoc) {
-                currWeight += Number.MAX_VALUE;
-            } else if (currPerm[iter] !== "DUMMY") {
-                console.log("responsemap", currLoc + currPerm[iter], responsesMap.get(currLoc + currPerm[iter]), responsesMap);
-
+            if (currPerm[iter] === "-1" && currLoc !== endingLoc) {
+                currWeight = Number.MAX_VALUE;
+            } else if (currPerm[iter] !== "-1") {
                 currWeight += locationGraph.get(currLoc).get(currPerm[iter]);
                 currPath.push(responsesMap.get(currLoc + currPerm[iter]));
             }
-
             currLoc = currPerm[iter];
         }
 
-        if (currLoc !== "DUMMY") {
+        if (currLoc !== "-1") {
             console.log("should not happen");
             currWeight += locationGraph.get(start_location).get(currLoc);
         }
@@ -276,7 +252,7 @@ function permutate(list) {
     function permuteRecurse(subList, recurseList) {
         var cur, recurseList = recurseList;
 
-        for (var iter = 0; iter < subList.length; iter++) {
+        for (let iter = 0; iter < subList.length; iter++) {
             cur = subList.splice(iter, 1);
             if (subList.length == 0) {
                 res.push(recurseList.concat(cur));
