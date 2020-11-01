a37565ef9c8ede5a365d7cdfeb8d38339b37eb80
jqiufu-ubc <jqiufu@alumni.ubc.ca>
2020-11-01 13:29:04 -0800

    added a test

--- front-end/app/src/androidTest/java/com/example/cpen321_wedo/ExampleInstrumentedTest.java
+++ front-end/app/src/androidTest/java/com/example/cpen321_wedo/ExampleInstrumentedTest.java
@@ -8,6 +8,11 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -22,5 +27,12 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.cpen321_wedo", appContext.getPackageName());
+    }
+
+    @Test
+    public void checkLoginButton() {
+        onView(withId(R.id.login)).perform(click());
+
+        onView(withId(R.id.loginInfoLayout)).check(matches(isDisplayed()));
     }
 }
