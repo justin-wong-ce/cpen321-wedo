2bf443c2d750bdde55519bb9686e092efe13057c
Ubuntu <cpen321@cpen321-wedo.walwv55x2eoe3p3q3yntzpzqtf.dx.internal.cloudapp.net>
2020-11-05 07:19:32 +0000

    added missing file

---
+++ backend/src/routers/routerHelper.js
@@ -0,0 +1,25 @@
+// Does error checking for MySQL callback and sends responses
+function callbackHandler(err, results) {
+
+    if (err.code === 'ER_DUP_ENTRY')
+        res.status(406).send("task list already exists");
+
+    else if (err.code === 'ER_WARN_NULL_TO_NOTNULL' ||
+        err.code === 'ER_WARN_DATA_OUT_OF_RANGE' ||
+        err.code === 'ER_WARN_DATA_TRUNCATED')
+        res.status(400).send("bad data format or type")
+
+    else if (err) {
+        res.status(404).send(err);
+    }
+
+    else {
+        res.status(200).send(results);
+    }
+}
+
+const routerHelper = {
+    callbackHandler: callbackHandler
+}
+
+module.exports = routerHelper
